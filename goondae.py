d4cae87e87c058a8cb8cf5ebef2e14287add378f
redundant4u <rafch@naver.com>
2018-04-18 17:15:05 +0900

    edited

--- goondae.py
+++ goondae.py
@@ -6,19 +6,21 @@ import requests
 from bs4 import BeautifulSoup
 
 def main():
-	texta = goondaeNotice()
-	print(texta)
-	#makeFile(text)
+	text = goondaeNotice()
+	#print(texta)
+	makeFile(text)
 
 def goondaeNotice():
+	text = ""
 	url = requests.get("https://www.mma.go.kr/contents.do?mc=mma0000743")
 	html = url.text
 	soup = BeautifulSoup(html, "html.parser")
-	src = soup.find_all("tr").text
-	encode = src
-	return encode
+	for info in soup.find_all("tr"):
+		text += str(info);
+	return text
 
 def makeFile(text):
+	text = str(text)
 	file = open("/var/www/html/notice/daegoon.txt", "w")
 	file.write(text)
 	file.close()
