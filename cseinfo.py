5595ed9ce409168603bea541821bcfdf36a1458a
redundant4u <rafch@naver.com>
2018-04-18 17:35:24 +0900

    more info edited

--- cseinfo.py
+++ cseinfo.py
@@ -10,25 +10,24 @@ def main():
 
 def cseNotice():
 	locationNumber = 0
-	url = requests.get("https://cse.pusan.ac.kr/cse/14651/subview.do")
-	html = url.text
-	soup = BeautifulSoup(html, "html.parser")
-	#src = soup.find_all("td", class_="_artclTdNum")
-	fileInit()
-	for noticeNumber in soup.find_all("td", class_="_artclTdNum"):
-		startNumber = isNumber(noticeNumber.text)
-		#print(locationNumber)
-		if(startNumber):
-			src = soup.find_all("tr")
-			for i in range(locationNumber + 1, 16):
-				title = src[i].find_all("td")[1].text
-				date = src[i].find_all("td")[3].text
-				for a in (src[i].find_all("a")):
-					link = "https://cse.pusan.ac.kr" + a['href']
-				processString(title, date, link)
-			break
-		else:
-			locationNumber += 1
+	for pageNumber in range(1, 2):
+		url = requests.get("https://cse.pusan.ac.kr/cse/14651/subview.do?page=" + str(pageNumber))
+		html = url.text
+		soup = BeautifulSoup(html, "html.parser")
+		fileInit()
+		for noticeNumber in soup.find_all("td", class_="_artclTdNum"):
+			startNumber = isNumber(noticeNumber.text)
+			if(startNumber):
+				src = soup.find_all("tr")
+				for i in range(locationNumber + 1, 16):
+					title = src[i].find_all("td")[1].text
+					date = src[i].find_all("td")[3].text
+					for a in (src[i].find_all("a")):
+						link = "https://cse.pusan.ac.kr" + a['href']
+					processString(title, date, link)
+				break
+			else:
+				locationNumber += 1
 	fileEnd()
 
 def isNumber(number):
@@ -39,19 +38,19 @@ def isNumber(number):
 		return False
 
 def fileInit():
-	file = open("/var/www/html/notice/info.php", "w")
+	file = open("3.php", "w")
 	file.write("<?php\n\t$info = array (\n\t\t'notice' =>\n\t\tarray (\n\t\t\t")
 
 def fileEnd():
-	file = open("/var/www/html/notice/info.php", "a")
+	file = open("3.php", "a")
 	file.write("\n\t\t),\n\t);\n\t$array = json_encode($info);\n\techo $array;\n?>")
 	file.close()
 
 def processString(string, date, link):
-	string = string.encode('utf8')
+	#string = string.encode('utf8')
 	fixedTitle = string.replace("새글", "")
 	fixedTitle2 = fixedTitle.strip()
-	file = open("/var/www/html/notice/info.php", "a")
+	file = open("3.php", "a")
 	file.write("array (\n\t\t\t\t'title' => '" + fixedTitle2 + "',\n\t\t\t\t'date' => '")
 	file.write(date + "',\n\t\t\t\t'link' => '")
 	file.write(link + "',\n\t\t\t),\n\t\t\t")
